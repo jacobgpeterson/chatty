d9a10fbc7ef13b2723be0731a42bbb01145a91cc
Jacob Peterson <tranquilwinter7@gmail.com>
2015-04-30 20:11:26 -0600

    finished steps 1-4

--- scripts/services/messageservice.js
+++ scripts/services/messageservice.js
@@ -1,7 +1,34 @@
 'use strict';
 
 angular.module('chattyApp')
-  .factory('MessageService', function MessageService() {
+  .service('MessageService', function MessageService($q, $http) {
     return {
+    	getMessages: function(){
+    		var dfd = $q.defer();
+
+    		$http({
+    			method: 'GET',
+    			url: 'http://localhost:8777'
+    		}).success(function(data){
+    			dfd.resolve(data);
+    		});
+    		return dfd.promise;
+    	},
+    	addMessage: function(message, date) {
+        var dfd = $q.defer();
+
+        $http({
+          method: 'POST',
+          url: 'http://localhost:8777',
+          data: {
+          	message: message,
+          	createdAt: date
+          }
+        }).success(function(data) {
+          dfd.resolve(data);
+        });
+
+        return dfd.promise;
+      	}
     }
-  });
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+var http = require('http');
+
+var messages = ["hello", "goodbye", "sandwich"]
+
+function onRequest(req, res){
+	if (req.method === 'GET') {
+    	res.writeHead(200, {
+      		'Connection': 'close',
+      		'Content-Type': 'application/json',
+      		'Access-Control-Allow-Origin': '*'
+    	});
+    	res.end(JSON.stringify(messages));
+  	}
+  	else if (req.method === 'POST') {
+    	var postData = '';
+    	req.on('data', function(chunk) {
+      		postData += chunk.toString();
+    	});
+    	req.on('end', function() {
+      		console.log(JSON.parse(postData));
+      		var msg = JSON.parse(postData);
+      		messages.push(msg);
+      		res.writeHead(200, {
+        		'Connection': 'close',
+        		'Content-Type': 'application/json',
+        		'Access-Control-Allow-Origin': '*'
+      		});
+      		res.end(JSON.stringify(messages));
+    	});
+  	}
+  	else if (req.method === 'OPTIONS') {
+    	res.writeHead(200, {
+        	'Connection': 'close',
+        	'Content-Type': 'application/json',
+        	'Access-Control-Allow-Origin': '*',
+        	'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        	'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      	});
+    	res.end("{}");
+  	}
+}
+
+http.createServer(onRequest).listen(8777)
